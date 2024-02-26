068fa8e0522cb4b99cb816619d1ab82441b70fc3
Jesper Andersson <Jesper.andersson@gritacademy.se>
2024-02-26 11:08:34 +0100

    gitignore

--- wp-content/themes/slutprojekt/functions.php
+++ wp-content/themes/slutprojekt/functions.php
@@ -1,2 +1,10 @@
 <?php
 require_once("Vite.php");
+
+require_once(get_template_directory() . "/init.php");
+
+function mytheme_add_woocommerce_support()
+{
+    add_theme_support('woocommerce');
+}
+add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
