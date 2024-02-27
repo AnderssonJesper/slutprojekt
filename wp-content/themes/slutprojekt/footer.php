2f526ebbdce57adc549eadf38c18614ea61a1bac
Noocce <noak.karlsson@gritacademy.se>
2024-02-27 13:39:47 +0100

    Footer

--- wp-content/themes/slutprojekt/footer.php
+++ wp-content/themes/slutprojekt/footer.php
@@ -1,17 +1,35 @@
 <footer>
-
-
-
-
-
-
-
-
-
-
-
-
-<body>
-    <?php wp_footer(); ?>
-</body>
+    <div class="footer-content">
+        <div class="footer-column footer-column-1">
+            <h2>URBAN OUTFITTERS</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            <div class="footer-links">
+                <a href="#link1">121 king street, Melbourne 3000</a>
+            </div>
+            <div class="footer-links">
+                <a href="#link2">+61 3 8376 6284</a>
+            </div>
+            <div class="footer-links">
+                <a href="#link3">contact@urbanoutfitters.com</a>
+            </div>
+            <div class="footer-icons">
+                <img src="http://slutprojekt.test/wp-content/uploads/2024/02/Vector-1.png" alt="Icon 1">
+                <img src="http://slutprojekt.test/wp-content/uploads/2024/02/vaadin_youtube.png" alt="Icon 2">
+                <img src="http://slutprojekt.test/wp-content/uploads/2024/02/ant-design_instagram-filled.png" alt="Icon 3">
+                <img src="http://slutprojekt.test/wp-content/uploads/2024/02/ant-design_twitter-outlined.png" alt="Icon 4">
+            </div>
+        </div>
+        <div class="footer-column footer-column-2">
+            <p>Blå kolumn (20%)</p>
+        </div>
+        <div class="footer-column footer-column-3">
+            <p>Gul kolumn (20%)</p>
+        </div>
+        <div class="footer-column footer-column-4">
+            <p>Röd kolumn (40%)</p>
+        </div>
+    </div>
+    <div class="footer-rights">
+        <p>Urban Outfitters © – All rights reserved</p>
+    </div>
 </footer>
