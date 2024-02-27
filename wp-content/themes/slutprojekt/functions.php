d936e7789c1500d65d70349c9c5cb3e522e0810b
Jesper Andersson <Jesper.andersson@gritacademy.se>
2024-02-27 09:39:07 +0100

    product-listing

--- wp-content/themes/slutprojekt/functions.php
+++ wp-content/themes/slutprojekt/functions.php
@@ -40,5 +40,55 @@ function theme_customize_register($wp_customize)
         'section' => 'header_settings',
         'type' => 'text',
     ));
+
+    $wp_customize->add_setting('banner_image', array(
+        'default' => '',
+        'sanitize_callback' => 'esc_url_raw',
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'banner_image', array(
+        'label' => __('Banner Image', 'mytheme'),
+        'section' => 'header_settings',
+        'settings' => 'banner_image',
+    )));
+
+    $wp_customize->add_setting('h1_text', array(
+        'default' => __('Default H1 Text', 'mytheme'),
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control('h1_text', array(
+        'label' => __('H1 Text', 'mytheme'),
+        'section' => 'header_settings',
+        'type' => 'text',
+    ));
+
+    $wp_customize->add_setting('h2_text', array(
+        'default' => __('Default H2 Text', 'mytheme'),
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control('h2_text', array(
+        'label' => __('H2 Text', 'mytheme'),
+        'section' => 'header_settings',
+        'type' => 'text',
+    ));
+
+    // P Settings
+    $wp_customize->add_setting('p_text', array(
+        'default' => __('Default P Text', 'mytheme'),
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control('p_text', array(
+        'label' => __('P Text', 'mytheme'),
+        'section' => 'header_settings',
+        'type' => 'text',
+    ));
+    
 }
 add_action('customize_register', 'theme_customize_register');
+
+
+
+
