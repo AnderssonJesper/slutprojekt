be1266f500a7e783fb84e2d504fd90efa2eb7bfc
Jesper Andersson <Jesper.andersson@gritacademy.se>
2024-03-05 18:43:17 +0100

    cart-finish

--- wp-content/themes/slutprojekt/functions.php
+++ wp-content/themes/slutprojekt/functions.php
@@ -571,10 +571,67 @@ add_action('woocommerce_archive_description', 'custom_bedroom_page_content');
 
 
 
+//Shortcode för related products i cart
 
 
+function custom_related_products_shortcode($atts)
+{
+    $atts = shortcode_atts(array(
+        'limit' => 2,
+        'left_arrow_image' => 'http://slutprojekt.test/wp-content/uploads/2024/03/left-arrow.png',
+        'right_arrow_image' => 'http://slutprojekt.test/wp-content/uploads/2024/03/right-arrow.png',
+        'title' => 'Also You May Buy',
+    ), $atts, 'custom_related_products');
 
+    $cart_items = WC()->cart->get_cart();
+    $related_product_ids = array();
 
+    foreach ($cart_items as $cart_item) {
+        $product_id = $cart_item['product_id'];
+        $related_products = wc_get_products(array(
+            'limit' => intval($atts['limit']),
+            'exclude' => array($product_id),
+            'orderby' => 'rand',
+            'return' => 'ids',
+        ));
 
+        $related_product_ids = array_merge($related_product_ids, $related_products);
+    }
 
+    $related_product_ids = array_unique($related_product_ids);
+    $related_products = wc_get_products(array(
+        'include' => $related_product_ids,
+    ));
+
+    if (!empty($related_products)) {
+        ob_start();
+        echo '<div class="related-products">';
+        echo '<h2>' . esc_html($atts['title']) . '</h2>';
+        echo '<div class="related-products-container">';
+
+        echo '<div class="related-product-arrow-left"><img src="' . esc_url($atts['left_arrow_image']) . '" alt="Left Arrow"></div>';
+
+        foreach ($related_products as $related_product) {
+            echo '<div class="related-product">';
+            echo '<a href="' . esc_url($related_product->get_permalink()) . '">' . $related_product->get_image() . '</a>';
+            echo '<h3>' . $related_product->get_name() . '</h3>';
+            if ($related_product->get_rating_count() > 0) {
+                echo '<div class="product-rating">' . wc_get_rating_html($related_product->get_average_rating(), $related_product->get_rating_count()) . '</div>';
+            } else {
+                echo '<div class="product-rating">' . wc_get_rating_html(0, 5) . '</div>';
+            }
+            echo '<p>' . $related_product->get_price_html() . '</p>';
+            echo '</div>';
+        }
+
+        echo '<div class="related-product-arrow-right"><img src="' . esc_url($atts['right_arrow_image']) . '" alt="Right Arrow"></div>';
+
+        echo '</div>'; 
+        echo '</div>'; 
+        return ob_get_clean();
+    } else {
+        return ''; 
+    }
+}
+add_shortcode('custom_related_products', 'custom_related_products_shortcode');
 
